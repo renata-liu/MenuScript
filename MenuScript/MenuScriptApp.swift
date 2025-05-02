1bec2397f4948326b05b96762547ab1004b64c59
Renata Liu <renata_liu@outlook.com>
2025-05-01 19:31:16 -0700

    Initial Commit

---
+++ MenuScript/MenuScriptApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuScriptApp.swift
+//  MenuScript
+//
+//  Created by Renata Liu on 2025-05-01.
+//
+
+import SwiftUI
+
+@main
+struct MenuScriptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
